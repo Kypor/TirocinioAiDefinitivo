5a425d261e93b335fe1bb46650630d1330fd6218
DiegoAlejandroSalazar <diegoalejndroflco@gmail.com>
2025-07-30 14:47:41 +0200

    messa traduzione parola

--- Assets/Scripts/DrawRandomIdeo.cs
+++ Assets/Scripts/DrawRandomIdeo.cs
@@ -13,12 +13,10 @@ public class DrawRandomIdeo : MonoBehaviour
     public Image partitionIdeoImage, ideoImage;
     private ListWrapper currentIdeo;
     private int currentNumberIndex = 0, currentIdeoIndex = 0, WordsCount = 0;
-    private int counterIdeo = 0;
-    public int numberOfWords = 1;
+   // public int numberOfWords = 1;
     public Image errorIndicator;
     [SerializeField]
     private TextMeshProUGUI text;
-
     [SerializeField] private TextMeshProUGUI ideoName;
     private CountdownTimer timer;
     private PointsManagerScript pointsManagerScript;
@@ -29,7 +27,6 @@ public class DrawRandomIdeo : MonoBehaviour
         timer = GetComponent<CountdownTimer>();
         ideoImage.sprite = GetRandomIdeo();
         pointsManagerScript = GetComponent<PointsManagerScript>();
-        counterIdeo = 0;
         
     }
 
@@ -53,20 +50,18 @@ public class DrawRandomIdeo : MonoBehaviour
 
     private Sprite GetRandomIdeo()
     {
-        if (WordsCount < numberOfWords)
+        if (WordsCount < japaneseIdeoArray.ideos.Count)
         {
 
             //int randomIndex = UnityEngine.Random.Range(0, availableIdeos.Count);
             currentIdeo = japaneseIdeoArray.ideos[WordsCount];
 
-            Debug.Log(WordsCount);
 
             //ideoName.text = currentIdeo.ideosInWord[currentNumberIndex].name;
             ideoName.text = currentIdeo.ideosInWord[0].name.Split("-")[1];
 
 
-            text.text = "Scrivi la parola: " + "'" + currentIdeo.word + "'" + " in giapponese";
-            //counterIdeo++;
+            text.text = "Scrivi la parola: " + "'" + currentIdeo.word + "'" + "\n in giapponese "+"\ntraduzione: " + currentIdeo.traduzione;
             return currentIdeo.ideosInWord[0];
             
         }
@@ -108,7 +103,6 @@ public class DrawRandomIdeo : MonoBehaviour
         WordsCount++;
         ideoImage.sprite = GetRandomIdeo();
         currentNumberIndex = 0;
-        //add point;
         pointsManagerScript.AddPoints(2);
     }
 
@@ -126,7 +120,6 @@ public class DrawRandomIdeo : MonoBehaviour
         errorIndicator.color = new Color(1, 0, 0, 1f);
         SoundManager.instance.PlaySoundFX(2);
         Debug.Log("sbagliato");
-        //sub points
         pointsManagerScript.SubPoints(2);
         return false;
     }
